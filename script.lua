3d9ae0aad070f23935abb291713b41517bb5f94f
L5 <100592537+L5-cg@users.noreply.github.com>
2022-10-04 22:14:58 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,71 +1,154 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/Revenant", true))()
 local Flags = Library.Flags
-local lp = game.Players.LocalPlayer
-local hum = game.Players.LocalPlayer.Character.Humanoid
 --Library.DefaultColor = Color3.fromRGB(65, 143, 232)
+
 local Window = Library:Window({
-    Text = "Midnight/Local"
+   Text = "Aiming"
 })
-Window:Slider({
-  Text = "Walk Speed",
-  Default = 16,
-  Minimum = 16,
-  Maximum = 250,
-  Callback = function(value)
-    while true do
-      hum.WalkSpeed = value
-      wait(0.1)
-    end
-  end
+
+local Window2 = Library:Window({
+   Text = "Fun"
 })
--- fill in with values that actually make sense V
-Window:Slider({
-  Text = "Jump Power",
-  Default = 0,
-  Minimum = 0,
-  Maximum = 0,
-  Callback = function(value)
-    while true do
-      hum.JumpHeight = value
-      wait(0.1)
-    end
-  end
+
+local Window3 = Library:Window({
+   Text = "Settings"
 })
+
 Window:Toggle({
-  Text = "Fly",
-  Callback = function(bool)
-    -- t/f 
-  end
-})
-Window:Toggle({
-  Text = "Noclip"
-  Callback = function(bool)
-    while true do
-      if bool == true then
-        --nc t
-      end
-      if bool == false then
-        --nc f
-      end
-      wait(0.5)
-    end
-  
+   Text = "Aimbot",
+   Callback = function()
+       print("Aimbot")
+   end
 })
 
 Window:Button({
-  Text = "Safe Reset"
-  Callback = function()
-    lp.Character.CFrame = CFrame.new(0, -0, 0)
-  end
+   Text = "Reload",
+   Callback = function(bool)
+       Library:Notification({
+           Text = "Your gun has been reloaded.",
+           Duration = 5
+       })
+   end
 })
-Window:Dropdown({
-  Text = "Stores",
-  List = {"Wood R us", "Land Store", "Boxed Cars", "Fancy Furnishings", "Bob's Shack", "Link's Logic", "Fine Arts Shop"},
-  Callback = function(bool)
-      
-  end
+
+Window:Toggle({
+   Text = "Silent-Aim",
+   Callback = function(bool)
+       print(bool)
+   end
 })
-Window:Dropdown({
-  Text = "Locations"
-  List = {"Spawn", "Volcano Entrance", "Volcano Top", "Ice Mountaintop", "Maze", "Maze End", "Swamp"},
+
+local Toggle = Window:Toggle({
+   Text = "No-Recoil",
+   Flag = "TestFlag",
+   Callback = function(bool)
+       print(bool)
+   end
 })
+
+Window2:Button({
+   Text = "Fling All",
+   Callback = function()
+       print("Flinging all")
+   end
+})
+
+Window2:Button({
+   Text = "Get Items",
+   Callback = function()
+       print("Got items")
+   end
+})
+
+Window2:Toggle({
+   Text = "Fly",
+   Callback = function()
+       print("weeeeeee")
+   end
+})
+
+Window2:Button({
+   Text = "Suicide",
+   Callback = function()
+       print("Don't do that please..")
+   end
+})
+
+Window2:Button({
+   Text = "God Mode",
+   Callback = function()
+       print("Nah you more like devil cuz you sexy asf")
+   end
+})
+
+Window3:Dropdown({
+   Text = "Color Scheme",
+   List = {"Dark", "White", "Aqua","Nova"},
+   Callback = function(bool)
+       print(bool)
+   end
+})
+
+Window3:Button({
+   Text = "Kill Client",
+   Callback = function()
+       game:Shutdown()
+   end
+})
+
+Window3:Slider({
+   Text = "Test Slider",
+   Default = 10,
+   Minimum = 1,
+   Maximum = 50,
+   Callback = function(value)
+       print(value)
+   end
+})
+
+local Label
+Window3:Prompt({
+   Text = "Join our discord!",
+   OnConfirm = function()
+       Library:Notification({
+           Text = "Thanks for joining our discord!",
+           Duration = 10
+       })
+       Label:Set({
+           Text = "Status: Joined Discord",
+           Color = Color3.fromRGB(56, 207, 154)
+       })
+   end
+})
+
+Label = Window3:Label({
+   Text = "Status: ...",
+   Color = Color3.fromRGB(214, 214, 214)
+})
+
+Window3:Keybind({
+   Text = "Toggle Library",
+   Default = Enum.KeyCode.F4,
+   Callback = function()
+       Library:Toggle()
+   end
+})
+
+Window3:Check({
+   Text = "Mode",
+   Callback = function()
+       warn("Why did i add this lol")
+   end
+})
+
+wait(2)
+Toggle:Set({
+   Bool = true
+})
+
+while true do
+   if Flags.TestFlag then
+       warn("Toggled")
+   end
+   wait(1)
+end
